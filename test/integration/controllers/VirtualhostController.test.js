d4acab3cb0185029d87550c6fbc08cbb444396ca
Genar Trias Ortiz <genar@acs.li>
2016-06-09 10:39:37 +0200

    playing with functional testing

--- test/integration/controllers/VirtualhostController.test.js
+++ test/integration/controllers/VirtualhostController.test.js
@@ -2,11 +2,27 @@ var request = require('supertest');
 
 describe('VirtualhostController', function() {
 
+  describe('#get()', function() {
+    it('should generate nginx config file', function (done) {
+      this.timeout(15000);
+      request(sails.hooks.http.app)
+        .get('/virtualhost')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) throw err;
+        })
+    });
+  });
+
   describe('#nginx()', function() {
     it('should generate nginx config file', function (done) {
+      this.timeout(15000);
       request(sails.hooks.http.app)
         .get('/virtualhost/nginx')
         .expect(200)
+        .end(function(err, res) {
+          if (err) throw err;
+        })
     });
   });
 
