0d1a7641eebb933b723ba74e8700979620fde5d8
Genar Trias Ortiz <genar@acs.li>
2016-06-11 20:26:43 +0200

    starting to play with mongo

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  mongoConnection: {
+     adapter: 'sails-mongo',
+     host: 'mongo',
+     port: 27017,
+     // user: 'username', //optional
+     // password: 'password', //optional
+     database: 'nginx-config-api' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
