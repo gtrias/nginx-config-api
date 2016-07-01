2b46509e9a00aaa8a89ceea8acd238c27e9945b5
Genar Trias Ortiz <genar@acs.li>
2016-07-01 17:16:44 +0200

    firsts tests

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var restify = require('restify');
 
-function virtualHostPost(req, res, next) {
+var server = restify.createServer();
+
+server.post('/virtualhost/:url', function virtualHostPost(req, res, next) {
   res.send('hello ' + req.params.name);
   next();
-}
-
-var server = restify.createServer();
-server.post('/virtualhost/:url', respond);
+});
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
---
+++ test/integration/virtualhost/post.test.js
@@ -0,0 +1,26 @@
+var restify = require('restify');
+var assert = require('assert');
+
+// init the test client
+var client = restify.createJsonClient({
+  url: 'http://127.0.0.1:8080',
+  version: '*'
+});
+
+// Test #1
+describe('201 response check', function() {
+    it('should get a 201 response', function(done) {
+        client.post('/virtualhost', { hello: 'world' }, function(err, req, res, data) {
+            if (err) {
+                throw new Error(err);
+            }
+            else {
+
+                if (data.code != 200) {
+                    throw new Error('invalid response from /post');
+                }
+                done();
+            }
+        });
+    });
+});
