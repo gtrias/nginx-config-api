d2877fc954ed2e9d2deff75702ff0ecec7026602
Genar Trias Ortiz <genar@acs.li>
2016-07-02 12:39:17 +0200

    first storage test

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var restify = require('restify');
 
 var server = restify.createServer();
 
+// Loading datastore
+var Datastore = require('nedb'),
+    db = new Datastore({ filename: 'data/hosts.db' });
+
+db.loadDatabase();
+
 server.put('/virtualhost/:url', function virtualHostPost(req, res, next) {
   console.log(req.params.url);
+  doc = {
+    name: req.params.url
+  }
+  db.insert(doc, function (err, newDoc) {   // Callback is optional
+    // newDoc is the newly inserted document, including its _id
+    // newDoc has no key called notToBeSaved since its value was undefined
+  });
   res.status(201);
   res.send(req.params.url);
   next();
